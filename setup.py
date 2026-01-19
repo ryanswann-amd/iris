6b90267178536943cdbc7b27c077581b2f049796
muhaawad <muhammadabdelghaffar.awad@amd.com>
2026-01-19 03:52:48 +0000

    Update project setup

--- setup.py
+++ setup.py
@@ -1,11 +1,58 @@
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (c) 2025-2026 Advanced Micro Devices, Inc. All rights reserved.
 
-from setuptools import setup
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import os
+import subprocess
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        cmake_args = [
+            f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}',
+            f'-DPython3_EXECUTABLE={sys.executable}',
+        ]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        cmake_args += [f'-DCMAKE_BUILD_TYPE={cfg}']
+        build_args += ['--', '-j4']
+
+        env = os.environ.copy()
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, 
+                              cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, 
+                              cwd=self.build_temp)
 
 # This setup.py provides backward compatibility for legacy metadata fields
 # that don't map directly from pyproject.toml's modern PEP 621 format.
 setup(
     url="https://rocm.github.io/iris/",
     author="Muhammad Awad, Muhammad Osama, Brandon Potter",
+    ext_modules=[CMakeExtension('iris._iris_vmem', sourcedir='.')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
 )
